b3fb7697c6e307077498277e90181abafc509249
Nic <ncumplido@msn.com>
2019-04-23 13:36:29 +0100

    Add sample code

--- app/src/main/java/com/example/a826488/arbaycampusinteresapp/CoFoFloor0.java
+++ app/src/main/java/com/example/a826488/arbaycampusinteresapp/CoFoFloor0.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 package com.example.a826488.arbaycampusinteresapp;
 
@@ -23,14 +24,9 @@ import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.sceneform.Node;
-import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
@@ -41,10 +37,9 @@ public class CoFoFloor0 extends AppCompatActivity {
     private static final String TAG = CoFoFloor0.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
 
-    private Node floorPlanCard;
-
     private ArFragment arFragment;
-    private ModelRenderable andyRenderable;
+    private Node floorPlan0Card;
+
 
     @Override
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
@@ -59,49 +54,23 @@ public class CoFoFloor0 extends AppCompatActivity {
 
         setContentView(R.layout.cofo_floor0);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        floorPlan0Card = new Node();
+        floorPlan0Card.setEnabled(false);
 
         // When you build a Renderable, Sceneform loads its resources in the background while returning
         // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+        ViewRenderable.builder()
+                .setView(this, R.layout.floor_0_plan)
+                .build()
+                .thenAccept(
+                        (renderable) -> {
+                            floorPlan0Card.setRenderable(renderable);
 
-        arFragment.setOnTapArPlaneListener(
-                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    if (andyRenderable == null) {
-                        return;
-                    }
-
-                    // Create the Anchor.
-                    /* Anchor anchor = hitResult.createAnchor();
-                    AnchorNode anchorNode = new AnchorNode(anchor);
-                    anchorNode.setParent(arFragment.getArSceneView().getScene());
-
-                    //Create the transformable andy and add it to the anchor.
-                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-                    andy.setParent(anchorNode);
-                    andy.setRenderable(andyRenderable);
-                    andy.select();*/
-
-                    if (floorPlanCard == null) {
-
-                        floorPlanCard = new Node();
-                        floorPlanCard.setEnabled(false);
-
-                        ViewRenderable.builder()
-                                .setView(this, R.layout.floor_0_plan)
-                                .build()
-                                .thenAccept(
-                                        (renderable) -> {
-                                            floorPlanCard.setRenderable(renderable);
-                                            ImageView imageView = (ImageView) renderable.getView();
-                                            //imageView.setImageResource("@drawable/floor_0_cofo");
-
-                                        })
-                                .exceptionally(
-                                        (throwable) -> {
-                                            throw new AssertionError("Could not load floorPlan0 card view.", throwable);
-                                        });
-                    }
-
-                });
+                        })
+                .exceptionally(
+                        (throwable) -> {
+                            throw new AssertionError("Could not load plane card view.", throwable);
+                        });
     }
 
     /**
